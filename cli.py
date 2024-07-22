85578947e2477d840f2ca74e5b19d6f84d728487
Dheison Gomes <dheisomgomes0@gmail.com>
2024-07-22 15:31:44 -0300

    CLI created and working

---
+++ cli.py
@@ -0,0 +1,87 @@
+import shutil
+from argparse import ArgumentParser, Namespace
+from tempfile import mktemp
+
+import whisper
+
+import lib
+
+
+def write_subtitles(file: str, segments: list[dict]):
+    out = open(file, "w")
+    out.write(lib.dump_srt_segments(segments))
+    out.close()
+
+
+def transcribe(
+    whisper_model: str, audio_file: str, src_language: str | None = None
+) -> dict:
+    lib.log(f"Loading whisper {whisper_model} model...")
+    model = whisper.load_model(whisper_model)
+    lib.log("Model loaded! Loading audio...")
+    audio = whisper.load_audio(audio_file)
+    lib.log("Audio loaded! Transcribing...")
+    result = model.transcribe(audio, language=src_language)
+    lib.log("Transcribed!")
+    return result
+
+
+def parse_args() -> Namespace:
+    argparser = ArgumentParser()
+    argparser.add_argument("--input", "-i", required=True, help="Input video file")
+    argparser.add_argument("--output", "-o", required=True, help="Output file")
+    argparser.add_argument(
+        "--target-language",
+        "-t",
+        default="en",
+        help="Output target language",
+    )
+    argparser.add_argument(
+        "--input-language", help="Set original movie language", default=None
+    )
+    argparser.add_argument(
+        "--whisper-model",
+        default="small",
+        help="Whisper model used to transcribe movies",
+    )
+    argparser.add_argument(
+        "--save-srt",
+        help="Save subtitles to <input>.<language>.srt file",
+        action="store_true",
+    )
+
+    args = argparser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    subtitles = transcribe(args.whisper_model, args.input, args.input_language)
+    temp_subtitles = mktemp()
+    write_subtitles(temp_subtitles, subtitles["segments"])
+    subtitles_basename = args.input.replace(args.input.split(".")[-1], "")[:-1]
+    if args.save_srt:
+        lib.log("Saving original subtitles...")
+        shutil.copy(temp_subtitles, f"{subtitles_basename}.{subtitles['language']}.srt")
+        lib.log("Saved!")
+
+    if subtitles["language"] != args.target_language:
+        lib.log(
+            f"Translating from {subtitles['language']} to {args.target_language}..."
+        )
+        subtitles = lib.translate_subtitles(subtitles, args.target_language)
+        write_subtitles(temp_subtitles, subtitles["segments"])
+
+    if args.save_srt:
+        lib.log(f"Saving {subtitles['language']} subtitles...")
+        shutil.copy(temp_subtitles, f"{subtitles_basename}.{subtitles['language']}.srt")
+        lib.log("Saved!")
+
+    lib.log("Adding subtitles to movie...")
+    lib.add_subtitles(args.input, temp_subtitles, args.output)
+    lib.log("Done!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ lib.py
@@ -0,0 +1,89 @@
+import os
+import subprocess
+from time import strftime
+from typing import Any
+
+import googletrans
+
+MAX_TEXT_LENGTH = 2000
+
+
+def log(text: str):
+    print(f"[{strftime('%H:%M:%S')}] {text}")
+
+
+def to_srt_time_format(time: float) -> str:
+    hours = int(time // 3600)
+    time -= hours * 3600
+    minutes = int(time // 60)
+    time -= minutes * 60
+    seconds = int(time // 1)
+    milliseconds = int((time - seconds) * 1000)
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}"
+
+
+def dump_srt_segments(segments: dict) -> str:
+    srt = ""
+    for idx, line in enumerate(segments):
+        srt += f"{idx+1}\n"
+        srt += f"{to_srt_time_format(line['start'])} --> {to_srt_time_format(line['end'])}\n"
+        srt += f"{line['text'].strip()}\n\n"
+    return srt
+
+
+def translate_subtitles(subtitle: dict[str, Any], dest: str = "pt"):
+    idx, separator = 0, "\n\n\n"
+    translated = subtitle.copy()
+    translated["text"] = ""
+    segments = subtitle.get("segments")
+    if not segments:
+        return subtitle
+    while idx < len("segments"):
+        start_idx, text = idx, ""
+        while len(text) < MAX_TEXT_LENGTH and idx < len(segments):
+            text += segments[idx]["text"] + separator
+            idx += 1
+        text = text.rstrip()
+        translated_text = translate_text(text, dest, subtitle.get("language"))
+        for piece_idx, text in enumerate(translated_text.split(separator)):
+            translated["segments"][start_idx + piece_idx]["text"] = text
+            translated["text"] += text
+    translated["language"] = dest
+    return translated
+
+
+def translate_text(text: str, dest: str, src: str = "auto") -> str:
+    translator = googletrans.Translator()
+    translated = translator.translate(text, dest, src)
+    return translated.text
+
+
+def add_subtitles(
+    movie: str,
+    subtitles: str,
+    output: str,
+    font="Roboto Medium",
+    font_size=11,
+    text_color="0000FFFF",
+    shadow_color="00000000",
+    shadow_size=1,
+    margin_bottom=35,
+):
+    video_filter = f"subtitles={subtitles}:force_style='FontName={font},FontSize={font_size},PrimaryColour=&H{text_color},OutlineColour=&H{shadow_color},BorderStyle=1,Shadow={shadow_size},MarginV={margin_bottom}'"
+    threads = os.cpu_count()
+    cmd = [
+        "ffmpeg",
+        "-threads",
+        str(threads),
+        "-i",
+        movie,
+        "-vf",
+        video_filter,
+        "-y",
+        output,
+    ]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    if result.returncode != 0:
+        raise Exception(
+            f"Failed to add subtitles directly to movie:\n\n{result.stderr}"
+        )
